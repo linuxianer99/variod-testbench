ebaa666766372260cf5e464c209b4a8edeecc6a1
Timo Bruderek <openvario@timoshome.org>
2020-05-18 10:05:58 -0400

    Improved docstrings to comply with PEP257

--- testbench/variodtb/sensordsim.py
+++ testbench/variodtb/sensordsim.py
@@ -13,6 +13,7 @@ VARIOD_PORT = 4353
 
 
 class NmeaGenerator:
+
     """Abstract interface for NMEA generators."""
 
     def generate(self) -> Generator[str, None, None]:
@@ -21,6 +22,7 @@ class NmeaGenerator:
 
 
 class FileGenerator(NmeaGenerator):
+
     """Generate arbitrary sentences by reading from the file."""
 
     def __init__(self, fname: str) -> None:
@@ -37,6 +39,7 @@ class FileGenerator(NmeaGenerator):
 
 
 class VarioSinGenerator(NmeaGenerator):
+
     """Synthesize sensord variometer readings.
 
     Iteratively go from minimum to maximum vario reading during given period
@@ -44,7 +47,8 @@ class VarioSinGenerator(NmeaGenerator):
     """
 
     def __init__(self, start: float, end: float, period: float) -> None:
-        """Create NMEA generator.
+        """
+        Create NMEA generator.
 
         start: minimum vario value
         end: maximum vario value
@@ -75,11 +79,11 @@ class VarioSinGenerator(NmeaGenerator):
 
 
 class SensordSim:
+
     """Sensor daemon simulator."""
 
     def __init__(self, nmea_gen: NmeaGenerator, delay: float) -> None:
         """Create simulator
-
         nmea_gen: NMEA sentence generator to use
         delay: delay between sentences in seconds
         """
@@ -87,7 +91,8 @@ class SensordSim:
         self.delay = delay
 
     def run(self) -> None:
-        """Start sensor simulation.
+        """
+        Start sensor simulation.
 
         Never finishes.
         """
